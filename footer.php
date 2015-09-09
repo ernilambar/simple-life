b3e52579521861acf01c1b5d8c84bb320c1b1115
Nilambar Sharma <nilambar@outlook.com>
2015-09-09 15:28:03 +0545

    fixing spacing in footer.php file

--- footer.php
+++ footer.php
@@ -6,6 +6,7 @@
  *
  * @package Simple Life
  */
+
 ?>
     </div> <!-- .row -->
 	</div><!-- #content -->
@@ -15,7 +16,7 @@
     'container_class' => 'container',
     'container_id'    => 'footer_widgets_wrap',
     );
-  simple_life_footer_widgets($args);
+  simple_life_footer_widgets( $args );
   ?>
 
 
@@ -23,21 +24,21 @@
 
   <?php
       wp_nav_menu( array(
-          'theme_location'    => 'footer',
-          'depth'             => 1,
-          'container'         => 'div',
-          'container_class'   => 'footer-nav-wrapper',
-          'menu_class'        => 'footer-nav',
-          'fallback_cb'       => '',
-          'link_after'        => '',
-          )
+        'theme_location'  => 'footer',
+        'depth'           => 1,
+        'container'       => 'div',
+        'container_class' => 'footer-nav-wrapper',
+        'menu_class'      => 'footer-nav',
+        'fallback_cb'     => '',
+        'link_after'      => '',
+        )
       );
   ?>
 
   <?php
     $copyright_text = simple_life_get_option( 'copyright_text' );
    ?>
-   <?php if ( ! empty( $copyright_text )): ?>
+   <?php if ( ! empty( $copyright_text ) ) : ?>
 
     <div id="copyright-wrap">
       <div class="copyright-text"><?php echo esc_html( $copyright_text ); ?></div>
@@ -49,12 +50,12 @@
     $powered_by = simple_life_get_option( 'powered_by' );
    ?>
 
-   <?php if ( true == $powered_by ): ?>
+   <?php if ( true == $powered_by ) : ?>
 
   		<div class="site-info" id="powered-by-wrap">
-  			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'simple-life' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'simple-life' ), 'WordPress' ); ?></a>
+  			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'simple-life' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'simple-life' ), 'WordPress' ); ?></a>
   			<span class="sep"> | </span>
-  			<?php printf( __( 'Theme: %1$s by %2$s.', 'simple-life' ), 'Simple Life', '<a href="http://www.nilambar.net/" rel="designer">Nilambar</a>' ); ?>
+  			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'simple-life' ), 'Simple Life', '<a href="http://www.nilambar.net/" rel="designer">Nilambar</a>' ); ?>
   		</div><!-- .site-info -->
 
    <?php endif ?>
