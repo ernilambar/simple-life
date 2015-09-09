789403cd114ed7ef00cc49ffd81d9276bc971dc1
Nilambar Sharma <nilambar@outlook.com>
2015-09-09 15:40:53 +0545

    space issue test in archive

--- archive.php
+++ archive.php
@@ -15,10 +15,10 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-        <?php
-          the_archive_title( '<h1 class="page-title">', '</h1>' );
-          the_archive_description( '<div class="taxonomy-description">', '</div>' );
-        ?>
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
@@ -26,7 +26,8 @@ get_header(); ?>
 
 				<?php
 
-					/* Include the Post-Format-specific template for the content.
+					/*
+					 * Include the Post-Format-specific template for the content.
 					 * If you want to override this in a child theme, then include a file
 					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 					 */
