97e4828ff7e218602bdf59519508a1bd1f04b8ba
Nilambar Sharma <nilambar@outlook.com>
2015-09-10 15:40:45 +0545

    ignore mean menu

--- js/custom.js
+++ js/custom.js
@@ -3,27 +3,27 @@
  *
  */
 
-jQuery( document ).ready(function($) {
+jQuery( document ).ready( function( $ ) {
 
-	$( '.widget' ).find( "ul" ).addClass( "list-unstyled" );
+	$( '.widget' ).find( 'ul' ).addClass( 'list-unstyled' );
 
-	jQuery( '#site-navigation' ).meanmenu({
-		meanScreenWidth: "640"
+	jQuery( '#site-navigation' ).meanmenu( {
+		meanScreenWidth: '640'
 	});
 
 	// Implement go to top.
 	if ( $( '#btn-scrollup' ).length > 0 ) {
 
-		$( window ).scroll(function(){
-			if ($( this ).scrollTop() > 100) {
+		$( window ).scroll( function() {
+			if ( $( this ).scrollTop() > 100 ) {
 				$( '#btn-scrollup' ).fadeIn();
 			} else {
 				$( '#btn-scrollup' ).fadeOut();
 			}
 		});
 
-		$( '#btn-scrollup' ).click(function(){
-			$( "html, body" ).animate( { scrollTop: 0 }, 600 );
+		$( '#btn-scrollup' ).click( function() {
+			$( 'html, body' ).animate( { scrollTop: 0 }, 600 );
 			return false;
 		});
 
