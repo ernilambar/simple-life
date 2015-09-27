8976ca0beb386250442e45e3b9da995c2b0a7ea8
ernilambar <nilambar@outlook.com>
2015-09-27 17:16:56 +0545

    fixing template files spacing issues

--- sidebar.php
+++ sidebar.php
@@ -6,34 +6,33 @@
  */
 
 ?>
-
 	<div id="secondary" <?php echo simple_life_sidebar_class( 'widget-area container clearfix' ); ?> role="complementary">
 		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
-      <?php
+		<?php
 
-        // Search Widget
-        $args = array(
-          'before_title'  => '<h3 class="widget-title">',
-          'after_title'   => '</h3>',
-        );
-        the_widget( 'WP_Widget_Search', array(), $args );
+		// Search Widget.
+		$args = array(
+		  'before_title'  => '<h3 class="widget-title">',
+		  'after_title'   => '</h3>',
+		);
+		the_widget( 'WP_Widget_Search', array(), $args );
 
-        // Archives Widget
-        $args = array(
-          'before_title'  => '<h3 class="widget-title">',
-          'after_title'   => '</h3>',
-        );
-        the_widget( 'WP_Widget_Archives', array(), $args );
+		// Archives Widget.
+		$args = array(
+		  'before_title'  => '<h3 class="widget-title">',
+		  'after_title'   => '</h3>',
+		);
+		the_widget( 'WP_Widget_Archives', array(), $args );
 
-        // Meta Widget
-        $args = array(
-          'before_title'  => '<h3 class="widget-title">',
-          'after_title'   => '</h3>',
-        );
-        the_widget( 'WP_Widget_Meta', array(), $args );
+		// Meta Widget.
+		$args = array(
+		  'before_title'  => '<h3 class="widget-title">',
+		  'after_title'   => '</h3>',
+		);
+		the_widget( 'WP_Widget_Meta', array(), $args );
 
-       ?>
+		?>
 
 		<?php endif; // End sidebar widget area. ?>
 	</div><!-- #secondary -->
--- support/woocommerce.php
+++ support/woocommerce.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Implement WooCommerce support.
+ *
+ * @package Simple Life
+ */
+
 add_theme_support( 'woocommerce' );
 
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
@@ -11,16 +17,16 @@ add_action( 'woocommerce_after_main_content', 'simple_life_theme_wrapper_end', 1
  * WooCommerce wrapper start.
  */
 function simple_life_theme_wrapper_start() {
-  echo '<section id="primary"';
-  echo simple_life_content_class( 'content-area' );
-  echo ' >';
-  echo '<main role="main" class="site-main" id="main">';
+	echo '<section id="primary"';
+	echo simple_life_content_class( 'content-area' );
+	echo ' >';
+	echo '<main role="main" class="site-main" id="main">';
 }
 
 /**
  * WooCommerce wrapper end.
  */
 function simple_life_theme_wrapper_end() {
-  echo '</main>';
-  echo '</section>';
+	echo '</main>';
+	echo '</section>';
 }
