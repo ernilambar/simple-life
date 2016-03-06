456c86078a51ca88ef245589b7b7e61341072005
ernilambar <nilambar@outlook.com>
2016-03-06 17:04:55 +0545

    fix search form button

--- searchform.php
+++ searchform.php
@@ -10,6 +10,6 @@
   <div>
     <label class="screen-reader-text" for="s"><?php esc_html_x( 'Search for:', 'label', 'simple-life' ); ?></label>
     <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="<?php echo esc_attr( simple_life_get_option( 'search_placeholder' ) ); ?>" class="search-field" />
-    <input type="submit" class="search-submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'simple-life' ); ?>" />
+    <input type="submit" class="search-submit screen-reader-text" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'simple-life' ); ?>" />
   </div>
 </form>
