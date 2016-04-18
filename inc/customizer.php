1fee9eb9812ccd035ada8ccc2e3cbce96882d04c
ernilambar <nilambar@outlook.com>
2016-04-18 15:02:18 +0545

    conditional hiding of logo options

--- inc/customizer.php
+++ inc/customizer.php
@@ -76,48 +76,52 @@ function simple_life_customize_register( $wp_customize ) {
 		)
 	);
 
-	// Logo Section.
-	$wp_customize->add_section( 'simple_life_options_logo',
-		array(
-			'title'      => __( 'Logo Options', 'simple-life' ),
-			'priority'   => 100,
-			'capability' => 'edit_theme_options',
-			'panel'      => 'simple_life_options_panel',
-		)
-	);
-	// Setting - site_logo.
-	$wp_customize->add_setting( 'simple_life_options[site_logo]',
-		array(
-			'default'           => $simple_life_default_options['site_logo'],
-			'capability'        => 'edit_theme_options',
-			'sanitize_callback' => 'esc_url_raw',
-		)
-	);
-	$wp_customize->add_control(
-		new WP_Customize_Image_Control(
-			$wp_customize,
-			'simple_life_options[site_logo]',
+	if ( ! function_exists( 'the_custom_logo' ) ) {
+		// Logo Section.
+		$wp_customize->add_section( 'simple_life_options_logo',
 			array(
-				'label'    => __( 'Logo', 'simple-life' ),
-				'section'  => 'simple_life_options_logo',
-				'settings' => 'simple_life_options[site_logo]',
-			 )
-		)
-	);
-	// Setting - replace_site_title.
-	$wp_customize->add_setting( 'simple_life_options[replace_site_title]',
-		array(
-			'default'           => $simple_life_default_options['replace_site_title'],
-			'capability'        => 'edit_theme_options',
-			'sanitize_callback' => 'simple_life_sanitize_checkbox',
-		)
-	);
-	$wp_customize->add_control('simple_life_options[replace_site_title]', array(
-		  'label'    => __( 'Replace Site Title by Logo', 'simple-life' ),
-		  'section'  => 'simple_life_options_logo',
-		  'type'     => 'checkbox',
-		  'priority' => 920,
-	));
+				'title'      => __( 'Logo Options', 'simple-life' ),
+				'priority'   => 100,
+				'capability' => 'edit_theme_options',
+				'panel'      => 'simple_life_options_panel',
+			)
+		);
+		// Setting - site_logo.
+		$wp_customize->add_setting( 'simple_life_options[site_logo]',
+			array(
+				'default'           => $simple_life_default_options['site_logo'],
+				'capability'        => 'edit_theme_options',
+				'sanitize_callback' => 'esc_url_raw',
+			)
+		);
+		$wp_customize->add_control(
+			new WP_Customize_Image_Control(
+				$wp_customize,
+				'simple_life_options[site_logo]',
+				array(
+					'label'    => __( 'Logo', 'simple-life' ),
+					'section'  => 'simple_life_options_logo',
+					'settings' => 'simple_life_options[site_logo]',
+				 )
+			)
+		);
+		// Setting - replace_site_title.
+		$wp_customize->add_setting( 'simple_life_options[replace_site_title]',
+			array(
+				'default'           => $simple_life_default_options['replace_site_title'],
+				'capability'        => 'edit_theme_options',
+				'sanitize_callback' => 'simple_life_sanitize_checkbox',
+			)
+		);
+		$wp_customize->add_control('simple_life_options[replace_site_title]', array(
+			  'label'    => __( 'Replace Site Title by Logo', 'simple-life' ),
+			  'section'  => 'simple_life_options_logo',
+			  'type'     => 'checkbox',
+			  'priority' => 920,
+		));
+
+	} // End if function_exists - the_custom_logo.
+
 
 	// General Section.
 	$wp_customize->add_section( 'simple_life_options_general',
