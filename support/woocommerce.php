d04254074f28ff629f4b9e6df1ae02d782cc4a8b
ernilambar <nilambar@outlook.com>
2016-04-18 16:00:09 +0545

    fix in woo support

--- support/woocommerce.php
+++ support/woocommerce.php
@@ -5,7 +5,16 @@
  * @package Simple_Life
  */
 
-add_theme_support( 'woocommerce' );
+add_action( 'after_setup_theme', 'simple_life_add_woocommerce_support' );
+
+/**
+ * Declare WooCommerce support.
+ */
+function simple_life_add_woocommerce_support() {
+
+    add_theme_support( 'woocommerce' );
+
+}
 
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
