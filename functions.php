9d595a0de27fa6ab339bfafaaee4da8c25de4974
ernilambar <nilambar@outlook.com>
2016-04-18 14:17:45 +0545

    declare logo support

--- functions.php
+++ functions.php
@@ -40,6 +40,11 @@ if ( ! function_exists( 'simple_life_setup' ) ) :
 		add_theme_support( 'title-tag' );
 
 		/*
+		 * Enable support for custom logo.
+		 */
+		add_theme_support( 'custom-logo' );
+
+		/*
 		 * Enable support for Post Thumbnails on posts and pages.
 		 */
 		add_theme_support( 'post-thumbnails' );
