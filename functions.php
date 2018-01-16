3fcdd5c4a2b9054fd46188f5d1320ee5bcb84b63
Nilambar Sharma <nilambar@outlook.com>
2018-01-16 16:06:45 +0545

    Fix html support

--- functions.php
+++ functions.php
@@ -62,7 +62,6 @@ if ( ! function_exists( 'simple_life_setup' ) ) :
 		 * to output valid HTML5.
 		 */
 		add_theme_support( 'html5', array(
-			'search-form',
 			'comment-form',
 			'comment-list',
 			'gallery',
