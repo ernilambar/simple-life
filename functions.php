94df06ad3ac373fc604e46b70bc58d537428f575
Nilambar Sharma <nilambar@outlook.com>
2022-07-09 15:39:58 +0545

    Update setup

--- functions.php
+++ functions.php
@@ -159,7 +159,7 @@ function simple_life_scripts() {
 
 	wp_enqueue_script( 'simple-life-meanmenu-script', get_template_directory_uri() . '/third-party/meanmenu/jquery.meanmenu' . $min . '.js', array( 'jquery' ), '2.0.6', true );
 	wp_enqueue_script( 'simple-life-custom', get_template_directory_uri() . '/js/custom' . $min . '.js', array( 'jquery', 'simple-life-meanmenu-script' ), SIMPLE_LIFE_VERSION, true );
-	wp_localize_script( 'simple-life-custom', 'Simple_Life_Screen_Reader_Text', array(
+	wp_localize_script( 'simple-life-custom', 'simpleLifeScreenReaderText', array(
 		'expand'   => __( 'expand menu', 'simple-life' ),
 		'collapse' => __( 'collapse menu', 'simple-life' ),
 	) );
--- scripts/customizer.js
+++ scripts/customizer.js
@@ -5,7 +5,6 @@
  */
 
 ( function( $ ) {
-
 	'use strict';
 
 	// Site title and description.
@@ -25,19 +24,18 @@
 		value.bind( function( to ) {
 			if ( 'blank' === to ) {
 				$( '.site-title a, .site-description' ).css( {
-					'clip': 'rect(1px, 1px, 1px, 1px)',
-					'position': 'absolute'
+					clip: 'rect(1px, 1px, 1px, 1px)',
+					position: 'absolute',
 				} );
 			} else {
 				$( '.site-title a, .site-description' ).css( {
-					'clip': 'auto',
-					'position': 'relative'
+					clip: 'auto',
+					position: 'relative',
 				} );
 				$( '.site-title a, .site-description' ).css( {
-					'color': to
+					color: to,
 				} );
 			}
 		} );
 	} );
-
-} )( jQuery );
+}( jQuery ) );
