65d9f0e1df55f931d2dd66f3fbbaf602f692404c
Nilambar Sharma <nilambar@outlook.com>
2024-07-25 16:39:11 +0545

    Update deply setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,9 @@
-// Env.
 require( 'dotenv' ).config();
 
-// Config.
 var rootPath = './';
-
-// Gulp.
 var gulp = require( 'gulp' );
-
-// Browser sync.
 var browserSync = require( 'browser-sync' ).create();
-
-// Rename.
 var rename = require( 'gulp-rename' );
-
-// Uglify.
 var uglify = require( 'gulp-uglify' );
 
 gulp.task( 'scripts', function() {
@@ -27,20 +17,14 @@ gulp.task( 'scripts', function() {
 gulp.task( 'watch', function() {
 	browserSync.init( {
 		proxy: process.env.DEV_SERVER_URL,
-		open: true,
+		open: false,
 	} );
 
-	// Watch CSS files.
 	gulp.watch( rootPath + 'style.css' ).on( 'change', browserSync.reload );
-
-	// Watch PHP files.
 	gulp.watch( rootPath + '**/**/*.php' ).on( 'change', browserSync.reload );
-
-	// Watch JS files.
 	gulp.watch( rootPath + 'scripts/*.js', gulp.series( 'scripts' ) ).on( 'change', browserSync.reload );
 } );
 
 // Tasks.
 gulp.task( 'default', gulp.series( 'watch' ) );
-
 gulp.task( 'build', gulp.series( 'scripts' ) );
