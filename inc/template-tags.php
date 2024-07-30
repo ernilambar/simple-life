6060c6fa5fdbc6ef4a36e4c69ec2021ba7770dc4
Nilambar Sharma <nilambar@outlook.com>
2024-07-30 16:35:41 +0545

    Remove custom logo

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -175,23 +175,12 @@ endif;
 if ( ! function_exists( 'simple_life_the_custom_logo' ) ) :
 
 	/**
-	 * Displays the optional custom logo.
+	 * Displays logo.
 	 *
 	 * @since 1.8
 	 */
 	function simple_life_the_custom_logo() {
-		if ( function_exists( 'the_custom_logo' ) ) {
-			the_custom_logo();
-		} else {
-			$site_logo = simple_life_get_option( 'site_logo' );
-			if ( ! empty( $site_logo ) ) {
-				?>
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="site-logo-link">
-					<img src="<?php echo esc_url( $site_logo ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" />
-				</a>
-				<?php
-			}
-		}
+		the_custom_logo();
 	}
 
 endif;
